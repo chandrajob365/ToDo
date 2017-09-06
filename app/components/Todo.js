26e6e3b6a283f0103bdbd219a907f932703b1f71
Manish <chandrajob365@gmail.com>
2017-09-07 02:45:43 +0530

    [Todo.js] Converted to presentation component

--- app/components/Todo.js
+++ app/components/Todo.js
@@ -1,23 +1,25 @@
-const React = require('react')
-const ReactDOM = require('react-dom')
+import React from 'react'
+// import ReactDOM from 'react-dom'
+// import {deleteTodo} from './actions/action'
+// import store from './store/store'
 
 class Todo extends React.Component {
-  constructor (props) {
-    super(props)
-    this.deleteTodo = this.deleteTodo.bind(this)
-  }
-
-  deleteTodo (e) {
-    this.props.deleteTodo(this.props.todoObj.id)
-  }
+  // constructor (props) {
+  //   super(props)
+  //   this.deleteTodo = this.deleteTodo.bind(this)
+  // }
+  //
+  // deleteTodo (e) {
+  //   store.dispatch(deleteTodo())
+  // }
 
   render (props) {
     return (
       <tr>
-        <td onClick={this.deleteTodo}>{this.props.todoObj.todo}</td>
+        <td onClick={this.props.onClick}>{this.props.todoObj.todo}</td>
       </tr>
     )
   }
 }
 
-module.exports = Todo
+export default Todo
