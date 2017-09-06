abfa1133566a5495d9c3081350f5b20e51367cca
Manish <chandrajob365@gmail.com>
2017-09-07 02:46:22 +0530

    [TodoCreate.js] Converted to presentation component

--- app/components/TodoCreate.js
+++ app/components/TodoCreate.js
@@ -1,5 +1,7 @@
-const React = require('react')
-const ReactDOM = require('react-dom')
+import React from 'react'
+// import ReactDOM from 'react-dom'
+// import {addToDo} from './actions/action'
+// import store from './store/store'
 
 class TodoCreate extends React.Component {
   constructor (props) {
@@ -8,7 +10,7 @@ class TodoCreate extends React.Component {
       value: ''
     }
     this.handleChange = this.handleChange.bind(this)
-    this.createNewToDo = this.createNewToDo.bind(this)
+    // this.createNewToDo = this.createNewToDo.bind(this)
   }
 
   handleChange (event) {
@@ -17,23 +19,25 @@ class TodoCreate extends React.Component {
     })
   }
 
-  createNewToDo (event) {
-    console.log('<TodoCreate, createNewToDo> this.state.value = ', this.state.value)
-    this.props.updateToDoList(this.state.value)
-    this.setState({
-      value: ''
-    })
-    ReactDOM.findDOMNode(this.refs.item).focus()
-  }
+  // createNewToDo (event) {
+  //   console.log('<TodoCreate, createNewToDo> this.state.value = ', this.state.value)
+  //   console.log('<TodoCreate, createNewToDo> this.refs.item.value = ', this.refs.item.value)
+  //   store.dispatch(addToDo(this.refs.item.value))
+  //   this.setState({
+  //     value: ''
+  //   })
+  //   ReactDOM.findDOMNode(this.refs.item).focus()
+  // }
 
   render (props) {
+    console.log('props = ', props)
     return (
       <div>
         <input type='text' ref='item' name='todo' value={this.state.value} onChange={this.handleChange} />
-        <button onClick={this.createNewToDo}>Create</button>
+        <button onClick={this.props.onAddToDoClick(this.state.value)}>Create</button>
       </div>
     )
   }
 }
 
-module.exports = TodoCreate
+export default TodoCreate
