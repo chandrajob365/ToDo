a17a60c73acfce036b45d17c13b4656c9faa7f75
Manish <chandrajob365@gmail.com>
2017-09-07 02:50:07 +0530

    [index.js] Modify to use provider and redux store

--- app/index.js
+++ app/index.js
@@ -1,51 +1,15 @@
-const React = require('react')
-const ReactDOM = require('react-dom')
-const TodoHeader = require('./components/TodoHeader')
-const TodoCreate = require('./components/TodoCreate')
-const TodoList = require('./components/TodoList')
+import React from 'react'
+import {render} from 'react-dom'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import todos from './reducers/reducer'
+import App from './components/App'
 
-class App extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      todoList: {},
-      currentTodoListId: 0
-    }
-    this.updateToDoList = this.updateToDoList.bind(this)
-    this.deleteTodo = this.deleteTodo.bind(this)
-  }
+let store = createStore(todos)
 
-  updateToDoList (newTodo) {
-    let newTodoId = ++this.state.currentTodoListId
-    this.setState(
-      {
-        todoList: Object.assign({}, this.state.todoList, {[newTodoId + newTodo]: {todo: newTodo, id: (newTodoId + newTodo)}}),
-        currentTodoListId: newTodoId
-      }
-    )
-  }
-
-  deleteTodo (todoId) {
-    let todoListCopy = Object.assign({}, this.state.todoList)
-    delete todoListCopy[todoId]
-    this.setState({
-      todoList: Object.assign({}, todoListCopy)
-    })
-  }
-
-  render () {
-    console.log('<index.js, render> Entry')
-    return (
-      <div>
-        <TodoHeader />
-        <TodoCreate updateToDoList={this.updateToDoList} />
-        <TodoList todoList={this.state.todoList} deleteTodo={this.deleteTodo} />
-      </div>
-    )
-  }
-}
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 )
