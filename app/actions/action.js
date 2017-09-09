33e08ef2a9e712218d0449a4e7c26189026945e4
Manish <chandrajob365@gmail.com>
2017-09-09 21:49:46 +0530

    [actions.js]Add updateTodo , showall , active, completed todo actions

--- app/actions/action.js
+++ app/actions/action.js
@@ -1,13 +1,40 @@
 export let addToDo = (text) => {
+  console.log('<action.js addToDo> text = ', text)
   return {
     type: 'ADD_TODO',
     text
   }
 }
 
-export let deleteTodo = (id) => {
+export let deleteTodo = () => {
   return {
-    type: 'DELETE_TODO',
-    id
+    type: 'DELETE_TODO'
+  }
+}
+
+export let updateToDoStatus = (id, status) => {
+  console.log('<action.js updateToDoStatus> id = ', id, '  status = ', status)
+  return {
+    type: 'UPDATE_STATUS',
+    id,
+    status
+  }
+}
+
+export let showAll = () => {
+  return {
+    type: 'ALL'
+  }
+}
+
+export let showActive = () => {
+  return {
+    type: 'ACTIVE'
+  }
+}
+
+export let showCompleted = () => {
+  return {
+    type: 'COMPLETED'
   }
 }
