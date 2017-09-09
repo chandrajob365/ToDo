68825207f38a3ac24086aa2df72188492637110e
Manish <chandrajob365@gmail.com>
2017-09-09 21:56:54 +0530

    [TodoList.js]Update props of Todo from delete to updateToDo

--- app/components/TodoList.js
+++ app/components/TodoList.js
@@ -1,33 +1,38 @@
 import React from 'react'
 import Todo from './Todo'
+import FlatButton from 'material-ui/FlatButton'
 
 class TodoList extends React.Component {
-
-  render (props) {
+  constructor (props) {
+    super(props)
+  }
+  render () {
+    console.log('<TodoList.js render> this.props.state = ', this.props.state)
     let rows = []
-    let todoList = this.props.todoList
-    if (todoList) {
-      for (let todoObj in todoList) {
-        if (todoList.hasOwnProperty(todoObj)) {
-          rows.push(<Todo
-            todoObj={todoList[todoObj]}
-            key={todoList[todoObj].id}
-            deleteTodo={() => this.props.ondeleteTodoClick(todoList[todoObj].id)} />)
-        }
+    let todoList = this.props.state.todoList
+    console.log('<TodoList>this.props.todoList = ', this.props.state.todoList)
+    for (let todoObj in todoList) {
+      if (todoList.hasOwnProperty(todoObj)) {
+        rows.push(
+          <Todo
+          todoObj={todoList[todoObj]}
+          key={todoList[todoObj].id}
+          updateToDoStatus={this.props.onUpdateTodoStatus} />)
       }
     }
     let styles = {
-      display: 'flex',
       justifyContent: 'center',
       border: '1px solid black',
       margin: '20px'
     }
 
     return (
-      <div>
-        <table style={styles}>
-          <tbody>{rows}</tbody>
-        </table>
+      <div style={styles}>
+        {rows}
+      <FlatButton label='ALL' disabled={true} />
+      <FlatButton label='Active' disabled={true} />
+      <FlatButton label='Completed' disabled={true} />
+      <FlatButton label='Delete' secondary={true} onClick={this.props.ondeleteTodoClick}  />
       </div>
     )
   }
