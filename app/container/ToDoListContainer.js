a6d1d9b431b174e81abaa2b3f01e1ebef5bb2717
Manish <chandrajob365@gmail.com>
2017-09-09 22:00:22 +0530

    [ToDoListContainer.js] Add property onUpdate to mapDispatchToProps

--- app/container/ToDoListContainer.js
+++ app/container/ToDoListContainer.js
@@ -1,18 +1,41 @@
 import {connect} from 'react-redux'
-import {deleteTodo} from '../actions/action'
+import {
+  deleteTodo,
+  updateToDoStatus,
+  showAll,
+  showActive,
+  showCompleted} from '../actions/action'
 import TodoList from '../components/TodoList'
-
 const mapStateToProps = (state) => {
+  console.log('<ToDoListContainer, mapStateToProps> -------- state = ', state)
   return {
-    todoList: state
+    state: state
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
+  console.log('<ToDoListContainer, mapDispatchToProps> dispatch = ', dispatch)
   return {
-    ondeleteTodoClick: id => {
-      dispatch(deleteTodo(id))
+    ondeleteTodoClick: () => {
+      console.log('<<ToDoListContainer, mapDispatchToProps , ondeleteTodoClick> Entry ')
+      console.log('ToDoListContainer, mapDispatchToProps> call to dispatch -> ', dispatch(deleteTodo()))
+    },
+    onUpdateTodoStatus: (id, status) => {
+      console.log('<ToDoListContainer, mapDispatchToProps- onUpdateTodoStatus>, id = ', id, '  status = ', status)
+      dispatch(updateToDoStatus(id, status))
     }
+    // onShowAllTodoClick: () => {
+    //   console.log('<<ToDoListContainer, mapDispatchToProps , onShowAllTodoClick> Entry ')
+    //   console.log('ToDoListContainer, mapDispatchToProps> call to dispatch -> ', dispatch(showAll()))
+    // },
+    // onShowActiveTodoClick: () => {
+    //   console.log('<<ToDoListContainer, mapDispatchToProps , onShowActiveTodoClick> Entry ')
+    //   console.log('ToDoListContainer, mapDispatchToProps> call to dispatch -> ', dispatch(showActive()))
+    // },
+    // onShowCompletedTodoClick: () => {
+    //   console.log('<<ToDoListContainer, mapDispatchToProps , onShowCompletedTodoClick> Entry ')
+    //   console.log('ToDoListContainer, mapDispatchToProps> call to dispatch -> ', dispatch(showCompleted()))
+    // }
   }
 }
 
