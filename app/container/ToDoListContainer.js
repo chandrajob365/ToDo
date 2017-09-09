624ec8aea3ae6b1cddd93854bc4b7b58ae01c95a
Manish <chandrajob365@gmail.com>
2017-09-10 04:20:34 +0530

    [ToDoListContainer.js] Add Visiblity filter related functions

--- app/container/ToDoListContainer.js
+++ app/container/ToDoListContainer.js
@@ -6,36 +6,65 @@ import {
   showActive,
   showCompleted} from '../actions/action'
 import TodoList from '../components/TodoList'
+
+const getDataByFilter = (state) => {
+  switch (state.filter) {
+    case 'ACTIVE':
+      return getActiveToDos(state)
+    case 'COMPLETED':
+      return getCompletedToDos(state)
+    default:
+      return state
+  }
+}
+
+const getActiveToDos = (state) => {
+  let ActiveToDos = {}
+  for (let key in state.todoList) {
+    if (state.todoList.hasOwnProperty(key)) {
+      if (!state.todoList[key].completed) {
+        Object.assign(ActiveToDos, {[key]: state.todoList[key]})
+      }
+    }
+  }
+  return { todoList: ActiveToDos, currentTodoListId: state.currentTodoListId, filter: 'ACTIVE' }
+}
+
+const getCompletedToDos = state => {
+  let completedToDos = {}
+  for (let key in state.todoList) {
+    if (state.todoList.hasOwnProperty(key)) {
+      if (state.todoList[key].completed) {
+        Object.assign(completedToDos, {[key]: state.todoList[key]})
+      }
+    }
+  }
+  return { todoList: completedToDos, currentTodoListId: state.currentTodoListId, filter: 'COMPLETED' }
+}
+
 const mapStateToProps = (state) => {
-  console.log('<ToDoListContainer, mapStateToProps> -------- state = ', state)
   return {
-    state: state
+    state: getDataByFilter(state)
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
-  console.log('<ToDoListContainer, mapDispatchToProps> dispatch = ', dispatch)
   return {
     ondeleteTodoClick: () => {
-      console.log('<<ToDoListContainer, mapDispatchToProps , ondeleteTodoClick> Entry ')
-      console.log('ToDoListContainer, mapDispatchToProps> call to dispatch -> ', dispatch(deleteTodo()))
+      dispatch(deleteTodo())
     },
     onUpdateTodoStatus: (id, status) => {
-      console.log('<ToDoListContainer, mapDispatchToProps- onUpdateTodoStatus>, id = ', id, '  status = ', status)
       dispatch(updateToDoStatus(id, status))
+    },
+    onShowAllTodoClick: () => {
+      dispatch(showAll())
+    },
+    onShowActiveTodoClick: () => {
+      dispatch(showActive())
+    },
+    onShowCompletedTodoClick: () => {
+      dispatch(showCompleted())
     }
-    // onShowAllTodoClick: () => {
-    //   console.log('<<ToDoListContainer, mapDispatchToProps , onShowAllTodoClick> Entry ')
-    //   console.log('ToDoListContainer, mapDispatchToProps> call to dispatch -> ', dispatch(showAll()))
-    // },
-    // onShowActiveTodoClick: () => {
-    //   console.log('<<ToDoListContainer, mapDispatchToProps , onShowActiveTodoClick> Entry ')
-    //   console.log('ToDoListContainer, mapDispatchToProps> call to dispatch -> ', dispatch(showActive()))
-    // },
-    // onShowCompletedTodoClick: () => {
-    //   console.log('<<ToDoListContainer, mapDispatchToProps , onShowCompletedTodoClick> Entry ')
-    //   console.log('ToDoListContainer, mapDispatchToProps> call to dispatch -> ', dispatch(showCompleted()))
-    // }
   }
 }
 
