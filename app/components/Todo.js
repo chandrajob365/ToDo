fd6c97497949a4fefc678a810bd25a773ec2af6c
Manish <chandrajob365@gmail.com>
2017-09-09 21:53:37 +0530

    [Todo.js] Add functions and state handler for material UI Checkbox

--- app/components/Todo.js
+++ app/components/Todo.js
@@ -1,23 +1,45 @@
 import React from 'react'
-// import ReactDOM from 'react-dom'
-// import {deleteTodo} from './actions/action'
-// import store from './store/store'
+import Checkbox from 'material-ui/Checkbox'
+
+const styles = {
+  block: {
+    maxWidth: 250,
+  },
+  checkbox: {
+    marginBottom: 16,
+  },
+}
 
 class Todo extends React.Component {
-  // constructor (props) {
-  //   super(props)
-  //   this.deleteTodo = this.deleteTodo.bind(this)
-  // }
-  //
-  // deleteTodo (e) {
-  //   store.dispatch(deleteTodo())
-  // }
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      checked: this.props.todoObj.complete
+    }
+    this.updateCheck = this.updateCheck.bind(this)
+  }
+
+  updateCheck (id) {
+    this.setState((oldState) => {
+      return {
+        checked: !oldState.checked
+      }
+    })
+    console.log('<todo.js updateCheck> id = ', id, '  !this.props.todoObj.completed = ', !this.props.todoObj.completed)
+    this.props.updateToDoStatus(id, !this.props.todoObj.completed)
+  }
 
   render (props) {
+    console.log('props = ', this.props)
     return (
-      <tr>
-        <td onClick={this.props.onClick}>{this.props.todoObj.todo}</td>
-      </tr>
+      <div >
+        <Checkbox
+          label={this.props.todoObj.todo}
+          checked={this.state.checked}
+          onCheck={() => { this.updateCheck(this.props.todoObj.id) }}
+          style={styles.checkbox} />
+      </div>
     )
   }
 }
