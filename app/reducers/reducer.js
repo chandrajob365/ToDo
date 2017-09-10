a399e12cd5666f96bb11068e641fbaf06167451d
Manish <chandrajob365@gmail.com>
2017-09-11 02:52:13 +0530

    [reducer.js] Remove global inital state and updated filter case

--- app/reducers/reducer.js
+++ app/reducers/reducer.js
@@ -1,23 +1,17 @@
-const intialState = {
-  todoList: {},
-  currentTodoListId: 0,
-  filter: ''
-}
-
-function todos (state = intialState, action) {
-  console.log('<reducer.js Entry>------state = ', state, '  -----------action = ', action)
+function todos (state = {}, action) {
+  console.log('state = ', state, '  action = ', action)
   switch (action.type) {
     case 'ADD_TODO':
-        let newToDoId = state.currentTodoListId
-        return {
-          ...state,
-          todoList : {
-            ...state.todoList,
-            [newToDoId + action.text]: {todo: action.text, id: (newToDoId + action.text), completed: false}
-          },
-          currentTodoListId: ++newToDoId,
-          filter: 'ALL'
-        }
+      let newToDoId = state.currentTodoListId || 0
+      return {
+        ...state,
+        todoList: {
+          ...state.todoList,
+          [newToDoId + action.text]: {todo: action.text, id: (newToDoId + action.text), completed: false}
+        },
+        currentTodoListId: ++newToDoId,
+        filter: 'ALL'
+      }
     case 'DELETE_TODO':
       let todoListCopy = Object.assign({}, state)
       for (let key in todoListCopy.todoList) {
@@ -27,10 +21,8 @@ function todos (state = intialState, action) {
           }
         }
       }
-      console.log('<reducer.js , DELETE_TODO>After Del todoListCopy = ', todoListCopy)
       return todoListCopy
     case 'UPDATE_STATUS':
-      console.log('<reducer.js, todos - UPDATE_STATUS > ------state = ', state, '  -----------action = ', action)
       return {
         ...state,
         todoList: {
@@ -42,20 +34,10 @@ function todos (state = intialState, action) {
         }
 
       }
-    case 'ALL':
+    case 'SET_VISIBILITY_FILTER':
       return {
         ...state,
-        filter: action.type
-      }
-    case 'ACTIVE':
-      return {
-        ...state,
-        filter: action.type
-      }
-    case 'COMPLETED':
-      return {
-        ...state,
-        filter: action.type
+        filter: action.filter
       }
     default:
       return state
