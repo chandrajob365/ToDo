642a94b56d7eb30df1b01d0640cca08e1411bceb
Manish <chandrajob365@gmail.com>
2017-09-11 04:18:21 +0530

    [index.js] Update changed name of root.js -> Root.js

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {render} from 'react-dom'
-import Root from './components/root'
+import Root from './components/Root'
 import configureStore from './configureStore'
 
 let store = configureStore()
